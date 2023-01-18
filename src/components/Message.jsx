4eb596184844d2441c06d8fd4c94abf608b49e48
William Leung <gwilliamleung@gmail.com>
2023-01-18 18:04:32 -0500

    added firebase connection + working ui

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -3,13 +3,13 @@ import React from 'react'
 const Message = () => {
   return (
     // <div className="flex flex-row-reverse p-2">
-    //     {/* <div>
+    //     <div>
     //         <img src="/Kaiju.png" className="w-16 h-16 rounded-full"/>      
     //         <span>Just now</span>
     //     </div>
     //     <div className="flex px-2 items-center ">
     //         <p className="bg-white py-2">Hello!</p>
-    //     </div> */}
+    //     </div>
     // </div>
  
     <div className="flex flex-row-reverse p-2">
--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -1,17 +1,29 @@
 import React from 'react'
+import Input from './Input'
 import Message from './Message'
 
-const Messages = () => {
+const Messages = ({message}) => {
   return (
-    <div className="bg-slate-600 h-[calc(100vh-19.4rem)] overflow-y-scroll">
-        <Message/>
-        <Message/>
-        <Message/>
-        <Message/>
-        <Message/>
-        <Message/>
-        <Message/>
-    </div>
+    <>
+        <div className="flex flex-row-reverse p-2">
+            <div>
+                <img src="/Kaiju.png" className="w-16 h-16 rounded-full"/>      
+                <span>{}</span>
+            </div>
+            <div className="flex  px-2 items-center">
+                <p className="bg-gray-50 py-2">{message.text}</p>
+            </div>
+        </div>
+      {/* <div className="flex flex-row-reverse p-2">
+        <div>
+            <img src="/Kaiju.png" className="w-16 h-16 rounded-full"/>      
+            <span>Just now</span>
+        </div>
+        <div className="flex px-2 items-center ">
+            <p className="bg-white py-2">Hello!</p>
+        </div>
+    </div> */}
+    </>
   )
 }
 
